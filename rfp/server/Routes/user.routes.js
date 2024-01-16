1a664def16182daa7f7e4b402c5c78fdbf42c8b9
mdivyanshalik <mdivyanshalik@gmail.com>
2024-01-17 03:13:20 +0530

    newwer code

--- rfp/server/Routes/user.routes.js
+++ rfp/server/Routes/user.routes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../Controllers/user.controller');
-const vendorController = require('../Controllers/vendor.controller');
+// const vendorController = require('../Controllers/vendor.controller');
 const categoryController = require('../Controllers/category.controller');
 const rfpController = require('../Controllers/rfp.controller');
 
 // User routes
-router.post('/login', userController.loginUser);
-router.post('/rfpCreate', userController.rfpCreate);
-router.post('/categoryCreate', userController.categoryCreate);
-router.post('/categoryRfpCreate', userController.categoryRfpCreate);
-router.post('/signup', userController.signupUser);
-router.get('/vendor', userController.vendorUser);
-router.get('/admin', userController.adminUser);
-router.get('/profile/:email', userController.getProfile);
-router.patch('/profile/:email', userController.updateProfile);
+// router.post('/login', userController.loginUser);
+// router.post('/rfpCreate', userController.rfpCreate);
+// router.post('/categoryCreate', userController.categoryCreate);
+// router.post('/categoryRfpCreate', userController.categoryRfpCreate);
+// router.post('/signup', userController.signupUser);
+// router.get('/vendor', userController.vendorUser);
+// router.get('/admin', userController.adminUser);
+// router.get('/profile/:email', userController.getProfile);
+// router.patch('/profile/:email', userController.updateProfile);
 
 
 
@@ -27,5 +27,6 @@ router.get('/getCategories', categoryController.getCategories);
 router.post('/createRFP', rfpController.createRFP);
 router.put('/updateRFP/:rfpId', rfpController.updateRFP);
 router.get('/getRFPs', rfpController.getRFPs);
+
 
 module.exports = router;
